6d17bcb96b03bd597c7c0b5e05753b5ebe7db287
Harser <jihiroh@mail.ru>
2021-06-19 18:02:24 +0300

    Creating site model

--- MusicPlatform/urls.py
+++ MusicPlatform/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from backend.views import DashboardView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', DashboardView.as_view())
 ]
